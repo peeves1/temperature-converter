027c0969e0f24a1946d1ff36e567b352f87ecfd7
peeves1 <manateemajestic@gmail.com>
2014-01-04 21:00:13 -0500

    First commit

---
+++ temperatureConverter/src/peeves/temperatureConverter/Converter.scala
@@ -0,0 +1,33 @@
+package peeves.temperatureConverter
+
+object Converter {
+    
+	def ask(): Unit = {
+	    print("Type \'F\'to convert to Fahrenheit, and \'C\' for Celsius: ")
+	}
+    
+    def main(args: Array[String]): Unit = {
+		println("TEMPERATURE CONVERTER")
+		println("~~~~~~~~~~~~~~~~~~~~~")
+		ask()
+	    val fc = readChar.toString()
+	    if(fc.toUpperCase() == "F") {
+	        println("You selected to convert to Fahrenheit.")
+	        print("Input degrees Celsius: ")
+	        var c1 = readInt
+	        var f1 = c1 * 9 / 5 + 32
+	        println("Degrees Fahrenheit: " + f1)
+	    }
+	    else if(fc.toUpperCase() == "C") {
+	        println("You selected to convert to Celsius.")
+	        print("Input degrees Fahrenheit: ")
+	        var c2 = readInt
+	        var f2 = (c2 - 32) * 5 / 9
+	        println("Degrees Celsius: " + f2)
+	    }
+	    else {
+	        println("You did not select a valid choice.")
+	    }
+	}
+    
+}
